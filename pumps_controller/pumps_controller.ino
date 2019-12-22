f765b8ae1d061d8542d10079dd03f057ac3f91c2
Filipe Barretto <filipebarretto@Ilha-Grande.local>
2019-12-22 15:38:31 -0300

    0.0.2

--- pumps_controller/pumps_controller.ino
+++ pumps_controller/pumps_controller.ino
@@ -7,38 +7,60 @@
 
 // CONSTANTS
 const int RELAY_PIN = 15;
-const int MOISTURE_SENSOR_PIN = 15;
+const int MOISTURE_SENSOR_PIN = 4;
 
-const int MOISTURE_THRESHOLD = 0.8;
+const int MOISTURE_THRESHOLD = 30;
 
-const int MOISTURE_CHECK_FREQUENCY = 1 * 60 * 1000;   // 1 MINUTE
-const int DATABASE_SYNC_FREQUENCY = 5 * 60 * 1000;    // 5 MINUTES 
+const int MOISTURE_CHECK_FREQUENCY = 10 * 60 * 1000;   // 10 MINUTES
+//const int DATABASE_SYNC_FREQUENCY = 5 * 60 * 1000;    // 5 MINUTES 
+const int WATER_PLANTS_DURATION = 800;    // 1 SECOND 
+
+
+float moisture;
 
 void setup() {
   Serial.begin(9600);
 
   // SETUP PINS
   pinMode(RELAY_PIN, OUTPUT);
-  pinMode(MOISTURE_SENSOR_PIN, INPUT);
+  digitalWrite(RELAY_PIN, HIGH);
 
 }
+
+
+/*
+ * 
+ * PAYLOAD:
+ * - TIMESTAMP
+ * - STRING SENSOR_ID
+ * - FLOAT MOISTURE
+ * - BOOL WATER_PLANTS
+ * 
+ */
 
 void loop() {
 
   // CHECK MOISTURE
-  moisture = analogRead(MOISTURE_SENSOR_PIN); 
+  moisture = (4096 - analogRead(MOISTURE_SENSOR_PIN)) / 4096.0 * 100.0; 
 
+  Serial.println("Soil Moisture: ");
   Serial.println(moisture);
+
+  // SEND DATA TO IOT CORE AND DYNAMODB DATABASE
   
   if (moisture < MOISTURE_THRESHOLD) {
     // WATERING PLANTS
     Serial.println("WATERING PLANTS");
+    
     digitalWrite(RELAY_PIN, LOW);
-    delay(1000);
-  
+    delay(WATER_PLANTS_DURATION);  
     digitalWrite(RELAY_PIN, HIGH);
+    
+  } else {
+    Serial.println("NOT WATERING PLANTS");
   }
+  
 
-  delay(1000);
+  delay(10000);
   
 }
